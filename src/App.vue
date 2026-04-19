12a97502c68f448defe5bff08b93f1037d838e16
Nikola-Limpet <2023465lim@aupp.edu.kh>
2026-04-19 14:02:20 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const message = ref("Task APP!");
+const newTask = ref("")
+
+</script>
+
+
+<template>
+  <main>
+    <h1> {{ message }}</h1>
+    <form>
+      <label >New Task</label>
+      <input name="newTask" v-model="newTask" placeholder="Enter a task" />
+      <button type="submit">Add Task</button>
+    </form>
+    <p> {{ newTask }}</p>
+  </main>
+</template>
+
+
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+
+}
+</style>
